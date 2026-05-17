6d1615305c983dfd830926ac0b34046d9b6041d5
Daniel <danielcoc356@gmail.com>
2026-05-17 13:24:58 -0500

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,25 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Eliza Studio",
-  description:
-    "Boutique digital para presentar catalogos, colecciones y novedades con una presencia elegante.",
+  title: "Catalogo de eliza",
+  description: "Catalogo de disponibilidad de productos de eliza.",
+  icons: "/logo.png",
+  openGraph: {
+    title: "Catálogo de Eliza",
+    description: "Catálogo de disponibilidad de productos de Eliza.",
+    url: "https://catalogo-eliza.vercel.app",
+    siteName: "Catálogo de Eliza",
+    images: [
+      {
+        url: "/logo.png",
+        width: 800,
+        height: 800,
+        alt: "Catálogo de Eliza",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
